3e66a3826a46fb2ec1ef7f0a4a7c66acc8fffb2a
Hyeongjun Kim <hyeongjun.kim@firstpenguine.school>
2023-10-09 19:44:25 +0900

    Add data files

---
+++ data/example_pokemon.py
@@ -0,0 +1,5 @@
+example_pokemon = {
+    "name": "알로라 디그다",
+    "types": ["땅", "강철"],
+    "image_url": "https://i.namu.wiki/i/zj-qqNv7XjCyKKxeTymdchMe8y9i0FMZyrspSEI9yCGqwMsXO-ag8e9Hu7AwBVq5fNuHOM4k2FUqmgFdJTfz7OSLZ1JcCopneY7JYHOx2N2g5_PFlrXhQ8-ADxsWb5skmrjsNN3Xa21OLqK35VKLFQ.webp",
+}
